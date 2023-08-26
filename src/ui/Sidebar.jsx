28f69251ec9efda004076a07101df08553ea80b2
Ivan Khokhlov <khokhlov.ivan@protonmail.com>
2023-08-26 21:37:59 +0200

    Started project

---
+++ src/ui/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { styled } from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1 / -1; // span from 1 row to the last one
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
+function Sidebar() {
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
+}
+
+export default Sidebar;
